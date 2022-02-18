5d0a154b79276de80c6fd8b3c3770da11bcfd22b
sdiallo <gsouleymane.diallo@gmail.com>
2022-02-18 21:18:08 +0000

    My 2meme commit on Git Manip Project 18022022

--- src/main/java/MainClass.java
+++ src/main/java/MainClass.java
@@ -2,5 +2,6 @@ public class MainClass {
 
     public static void main(String[] args) {
         System.out.println("Je suis dans le Projet....");
+        System.out.println("Je suis le deuxieme changement a etre fait");
     }
 }
